a001d7978baafc6f86b2cb0987a93488da8785ac
allo0001 <allo0001@yandex.ru>
2021-02-09 18:14:35 +0300

    Добавил решенное ДЗ

---
+++ hw_3.py
@@ -0,0 +1,9 @@
+
+for num in range(1, 21):
+    if num == 1:
+        print(f'{num} процент')
+    elif (num >= 2 and num <= 4):
+        print(f'{num} процента')
+    elif num >= 5 :
+        print(f'{num} процентов')
+
