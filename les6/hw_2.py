5c9ace939fb2993e6833ac59d41f5a6d1fc49464
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-03-02 22:57:19 +0300

    4

--- les6/hw_2.py
+++ les6/hw_2.py
@@ -1,3 +1,12 @@
+def getSpammer(logs: list):
+    res = {}
+    for i in logs:
+        res.setdefault(i[0], 0)
+        res[i[0]] += 1
+    m = max(res.values())
+    return {i:v for i,v in res.items() if v == m}
+
+
 logs =[]
 
 with open('nginx_logs.txt', 'r') as f:
@@ -7,4 +16,7 @@ with open('nginx_logs.txt', 'r') as f:
             break
         s = s.split('"')[:2]
         logs.append((s[0].split(' ')[0], s[1].split(' ')[0], s[1].split(' ')[1]))
-    print(logs)
+    
+print(getSpammer(logs))
+
+
