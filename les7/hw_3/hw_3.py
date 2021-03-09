36f2ab38a8e9f78b029a669a921507442da4bb7b
allo0001 <allo0001@yandex.ru>
2021-03-09 12:25:37 +0300

    Update hw_3.py

--- les7/hw_3/hw_3.py
+++ les7/hw_3/hw_3.py
@@ -1,5 +1,6 @@
 import json
 import os
+from shutil import copyfile
 
 
 def create_folder(path):
@@ -20,8 +21,19 @@ def iter_tree(folders, path):
             create_file(os.path.join(path, folder))
 
 
-PATH = r'E:\git\first_course\first_course\les7\hw_1_2'
+PATH = r'C:\Users\loktionov\Documents\GitHub\firs_course\les7\hw_3'
 CONFIG = 'foldrs_tree_2.json'
 
 with open(CONFIG, 'r') as f:
     iter_tree(json.load(f), PATH)
+
+mp_path = os.path.join(PATH,'my_project')
+create_folder(os.path.join(mp_path, 'templates'))
+for i in os.walk(mp_path):
+    if i[2]:
+        folder_name = os.path.split(i[0])[1]
+        if os.path.split(i[0])[0] != os.path.join(mp_path, 'templates'):
+            for file in i[2]:
+                if file.endswith('.html'):
+                    create_folder(os.path.join(mp_path,'templates',folder_name))
+                    copyfile(os.path.join(i[0],file), os.path.join(mp_path,'templates',folder_name,file))
