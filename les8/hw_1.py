47f4cda822d6391c979bbe5c94483ef471e3a98b
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-03-10 22:25:27 +0300

    Update hw_1.py

---
+++ les8/hw_1.py
@@ -0,0 +1,16 @@
+import re
+
+
+def email_parse(email):
+    re_mail = re.compile(r'\w+[@]\w+[.]\w+')
+    if re_mail.search(email):
+        re_u = re.search(r'\w+[@]', email)
+        re_d = re.search(r'[@]\w+[.]\w+', email)
+        return {'username': re_u[0][:-1], 'domain': re_d[0][1:]}
+    else:
+        raise ValueError
+
+
+print(email_parse('someone@geekbrains.ru ds'))
+
+print(email_parse('someone@geekbrainsru ds'))
