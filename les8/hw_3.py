771eb09d4eec6a3ed11a6084339ba0e04e8d89f7
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-03-12 21:10:57 +0300

    Update hw_3.py

--- les8/hw_3.py
+++ les8/hw_3.py
@@ -1,1 +1,25 @@
-pass
+from functools import wraps
+
+
+def type_logger(func):
+
+    @wraps(func)
+    def wraper(*args, **kvargs):
+        types = ({a: type(a) for a in args})
+        if kvargs:
+            print(f'{func.__name__} args: {types}, kvargs: {kvargs}')
+        else:
+            print(f'{func.__name__ } {types}')
+        res = func(*args, **kvargs)
+        return res
+
+    return wraper
+
+
+@type_logger
+def calc_cube(x, y='15'):
+   return x ** 3
+
+
+a = calc_cube(5, y=6)
+print(calc_cube)
