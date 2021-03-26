ccece3d01af726264b1c876f8be0f317eb519c96
allo0001 <77399306+allo0001@users.noreply.github.com>
2021-03-26 23:00:39 +0300

    Update hw_1.py

--- les11/hw_1.py
+++ les11/hw_1.py
@@ -1,6 +1,31 @@
-# Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
-# В рамках класса реализовать два метода. Первый — с декоратором @classmethod. 
-# Он должен извлекать число, месяц, год и преобразовывать их тип к типу «Число». 
-# Второй — с декоратором @staticmethod, должен проводить валидацию числа,
-# месяца и года (например, месяц — от 1 до 12). 
-# Проверить работу полученной структуры на реальных данных.
+import re
+
+
+class Date:
+    def __init__(self, date):
+        self.date = date
+
+    @classmethod
+    def pars(cls, date):
+        if d := cls.valid(date):
+            return map(int, d.split('-'))
+
+    @staticmethod
+    def valid(date):
+        if re.match(r'^\d{4}(-\d{2}){2}$', date):
+            y, m, d = map(int, date.split('-'))
+            if 2100 >= y >= 1900 and 0 < m < 13 and 0 < d < 31:
+                return date
+            else:
+                print('Ошибка формата даты.')
+                return False
+        else:
+            print('Ошибка формата даты.')
+            return False
+
+
+d1 = '2021-05-26'
+d2 = '2021-05-26'
+
+print(Date.valid('2021-05-01'))
+print(*Date.pars('2021-05-01'))
